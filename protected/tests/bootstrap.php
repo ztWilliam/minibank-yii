cb5e755329476714403c63501dacd7bf957b447d
Lin Weiwei <linww@minibank.me>
2015-04-16 18:32:07 +0800

    modify the bootstrap referenced by the yii framework bootstrap options.

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -5,6 +5,18 @@ $yiit=dirname(__FILE__).'/../../vendor/yiisoft/yii/framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 require_once($yiit);
-//require_once(dirname(__FILE__).'/WebTestCase.php');
+require_once(dirname(__FILE__).'/WebTestCase.php');
 
-Yii::createWebApplication($config);
+// Support PHPUnit <=3.7 and >=3.8
+if (@include_once('PHPUnit/Framework/TestCase.php')===false) // <= 3.7
+    require_once('src/Framework/TestCase.php'); // >= 3.8
+
+
+// make sure non existing PHPUnit classes do not break with Yii autoloader
+Yii::$enableIncludePath = false;
+Yii::setPathOfAlias('tests', dirname(__FILE__));
+Yii::import('tests.*');
+
+new TestApplication($config);
+
+//Yii::createWebApplication($config);
