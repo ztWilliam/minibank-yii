6e9ab1461c1079ee37702ae76000c656b9934eeb
Lin Weiwei <linww@minibank.me>
2015-04-19 20:06:29 +0800

    Let the test config file import the common classes' paths.

--- protected/config/test.php
+++ protected/config/test.php
@@ -3,6 +3,17 @@
 return CMap::mergeArray(
 	require(dirname(__FILE__).'/main.php'),
 	array(
+        // autoloading model and component classes
+        'import'=>array(
+            'application.models.*',
+            'application.components.*',
+
+            'application.modules.wechat.components.*',
+            'application.modules.wechat.components.baseUtil.*',
+            'application.modules.wechat.components.wxUtil.*',
+            'application.modules.wechat.components.crmUtil.*',
+        ),
+
         'components'=>array(
             'fixture'=>array(
                 'class'=>'system.test.CDbFixtureManager',
