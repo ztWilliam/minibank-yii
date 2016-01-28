82a6cde740fce7e8527a673b9ee198a9d54c6adf
Lin Weiwei <linww@minibank.me>
2016-01-28 17:09:28 +0800

    Add a test action to check whether the oAuth link will be correctly redirected by WeChat server.

--- protected/modules/bank/controllers/BankAdminController.php
+++ protected/modules/bank/controllers/BankAdminController.php
@@ -6,6 +6,13 @@
  * Time: 上午11:42
  */
 
-class BankAdminController {
+class BankAdminController  extends Controller{
+
+    public function actionWizard(){
+        LogWriter::warning('Welcome to bank creation wizard');
+        LogWriter::warning('Hello wechat code : ' . $_GET['code']);
+
+        echo 'Hi, this is a test page';
+    }
 
 }
